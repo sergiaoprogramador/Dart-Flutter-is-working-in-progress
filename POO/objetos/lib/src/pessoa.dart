cc7430b610122739b249f782af308cdc75584fec
Sergio Ramos <sergio.ramos@enesolucoes.com.br>
2021-02-28 17:24:51 -0400

    Encapsulamentos

--- POO/objetos/lib/src/pessoa.dart
+++ POO/objetos/lib/src/pessoa.dart
@@ -3,6 +3,12 @@ class Pessoa {
   int idade;
   String sexo;
 
+  // Variavel local (private)
+  String _nomeLocal = "Apenas acessada de dentro da classe";
+
+  // Variavel instanciada apenas uma vez
+  final String outroNome = "Ramos";
+
   // forma abreviada do construtor
   Pessoa({this.nome, this.idade, this.sexo});
 }
